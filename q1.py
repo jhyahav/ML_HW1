c2283022a764af801a5f86ea1fb70d2735773b39
Jonathan <jhyahav2@gmail.com>
2023-03-19 10:24:02 +0200

    progress on 1

--- q1.py
+++ q1.py
@@ -1,1 +1,16 @@
-import numpy
+import numpy as np
+
+N = 200000
+n = 20
+matrix = np.random.randint(0, 2, (N, n))
+row_means = matrix.sum(axis=1) / n
+
+
+eps = np.linspace(0, 1, 50)
+probs = [0] * len(eps)
+for i in range(len(eps)):
+    cnt = 0
+    for mean in row_means:
+        cnt += abs(mean - 0.5) > eps[i]
+    probs[i] = cnt / N
+np_probs = np.array(probs)
